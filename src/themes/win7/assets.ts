15512d2367c908f2aadf1da7c4af71ef78fc4617
c4m1r <c4m1rr@gmail.com>
2026-09-08 23:41:47 +0500

    feat(win7): define imported theme assets

---
+++ src/themes/win7/assets.ts
@@ -0,0 +1,31 @@
+import bootVideo from './assets/boot/win7-boot-anim.mp4';
+import startupSound from './assets/audio/windows-7-startup.mp3';
+import loginBackground from './assets/images/login-background.jpg';
+import desktopWallpaper from './assets/images/desktop-wallpaper.jpg';
+import windowsLogo from './assets/images/windows-7-logo.png';
+import startOrb from './assets/images/start-orb.png';
+import startOrbHover from './assets/images/start-orb-hover.png';
+import userAvatar from './assets/images/user-avatar.png';
+import shutdownIcon from './assets/images/shutdown.png';
+import volumeIcon from './assets/icons/volume.png';
+import computerIcon from './assets/icons/computer.png';
+import internetExplorerIcon from './assets/icons/internet-explorer.png';
+import mailIcon from './assets/icons/mail.png';
+import folderIcon from './assets/icons/folder.png';
+
+export const WIN7_ASSETS = {
+  bootVideo,
+  startupSound,
+  loginBackground,
+  desktopWallpaper,
+  windowsLogo,
+  startOrb,
+  startOrbHover,
+  userAvatar,
+  shutdownIcon,
+  volumeIcon,
+  computerIcon,
+  internetExplorerIcon,
+  mailIcon,
+  folderIcon,
+} as const;
