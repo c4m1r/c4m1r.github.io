f411b36621b5eb54103be3158ffd571a6105548a
c4m1r <c4m1rr@gmail.com>
2026-09-11 13:36:23 +0500

    refactor(winxp): reuse authentic XP identity mark

---
+++ src/themes/winxp/xpIdentityLogo.ts
@@ -0,0 +1,17 @@
+import bootLogoPart0 from './assets/boot/xp-mark.b64.0.txt?raw';
+import bootLogoPart1 from './assets/boot/xp-mark.b64.1.txt?raw';
+import bootLogoPart2 from './assets/boot/xp-mark.b64.2.txt?raw';
+import bootLogoPart3 from './assets/boot/xp-mark.b64.3.txt?raw';
+import bootLogoPart4 from './assets/boot/xp-mark.b64.4.txt?raw';
+import bootLogoPart5 from './assets/boot/xp-mark.b64.5.txt?raw';
+import bootLogoPart6 from './assets/boot/xp-mark.b64.6.txt?raw';
+
+export const XP_IDENTITY_LOGO = `data:image/png;base64,${[
+  bootLogoPart0,
+  bootLogoPart1,
+  bootLogoPart2,
+  bootLogoPart3,
+  bootLogoPart4,
+  bootLogoPart5,
+  bootLogoPart6,
+].join('')}`;
